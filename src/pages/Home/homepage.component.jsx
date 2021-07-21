fbbe5ababffa2f766b1a65579cc981c84fac1b95
kmiqbal19 <km.iqbal01@gmail.com>
2021-07-21 23:14:47 +0200

    added typing para in the hero section, implemented by react-typical. And finally changed the stylings.

--- src/pages/Home/homepage.component.jsx
+++ src/pages/Home/homepage.component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-
+import Typical from "react-typical";
 import "./homepage.component.css";
 import ThreeDElements from "./Scene/threeD.component.jsx";
+
 function HomePage() {
   return (
     <>
@@ -10,7 +11,20 @@ function HomePage() {
           <h3 className="hero-name">
             <span>{`</`}</span>K M Iqbal<span>{`>`}</span>
           </h3>
-          <h1 className="hero-occupation">Frontend Developer</h1>
+
+          <p className="typing-para">
+            <Typical
+              steps={[
+                "A Frontend Developer",
+                4000,
+                "An Urban Planner",
+                2000,
+                "A M.Sc Student",
+                2000,
+              ]}
+              loop={Infinity}
+            />
+          </p>
         </div>
         <ThreeDElements />
       </section>
