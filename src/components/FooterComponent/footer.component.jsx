fb026fa3eadf57a38332e94917aa947ab6555c48
kmiqbal19 <km.iqbal01@gmail.com>
2021-08-18 21:18:42 +0200

    updated footer component and made it responsive

--- src/components/FooterComponent/footer.component.jsx
+++ src/components/FooterComponent/footer.component.jsx
@@ -1,9 +1,66 @@
-import React, { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useState, useEffect, useRef } from "react";
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./footer.component.css";
+import stateBuilt from "./stateBuiltwith";
 function FooterComponent() {
-  return <div className="footer-container"></div>;
+  const [infoClick, setInfoClicked] = useState(false);
+  const clickHandler = () => setInfoClicked(!infoClick);
+
+  const BuiltWith = () => {
+    return stateBuilt.map((tool, index) => {
+      return (
+        <div key={index} className="tool-container--footer">
+          <div className="toolLogo-container__builtwith--footer">
+            <img
+              className="toolLogo__builtwith--footer"
+              src={tool.toolLogo}
+              alt={tool.logoName}
+            />
+          </div>
+          <div className="toolName__builtwith--footer">{tool.toolName}</div>
+          <div className="toolDescription__builtwith--footer">
+            {tool.toolDescription}
+          </div>
+        </div>
+      );
+    });
+  };
+  return (
+    <div className="footer-container">
+      <div className="losefear-footer">
+        <span className="thankyou__footer">
+          Thank you for checking out my website!
+        </span>
+        <span className="losefear-footer__text">
+          {"// To be creative, lose the fear of being wrong.."}
+        </span>
+      </div>
+      <div className="designed-footer">
+        <span className="designed-footer__text">Portfolio 2021</span>
+        <span className="designed-footer__text">
+          &#169; Designed and developed by me{" "}
+          <span
+            className="info-builtwith__footer"
+            onClick={() => clickHandler()}
+          >
+            info
+          </span>
+          .
+        </span>
+        <div
+          className={
+            !infoClick ? "built-with__footer" : "built-with__footer hidden"
+          }
+        >
+          <span className="builtwith-footer__text">
+            Built with <i class="far fa-heart"></i>
+          </span>
+          <BuiltWith />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FooterComponent;
---
+++ src/components/FooterComponent/stateBuiltwith.js
@@ -0,0 +1,25 @@
+import reactLogo from "../../assets/builtwith/react.png";
+import figmaLogo from "../../assets/builtwith/figma.png";
+import gsapLogo from "../../assets/builtwith/greensock.png";
+
+const stateBuilt = [
+  {
+    toolLogo: figmaLogo,
+    toolName: "Figma",
+    logoName: "figma-logo",
+    toolDescription: "Design Tool",
+  },
+  {
+    toolLogo: reactLogo,
+    logoName: "react-logo",
+    toolName: "React",
+    toolDescription: "JS Library",
+  },
+  {
+    toolLogo: gsapLogo,
+    toolName: "GSAP",
+    logoName: "gsap-logo",
+    toolDescription: "Animation",
+  },
+];
+export default stateBuilt;
